68329869955be14c0dae1d21f8e6b125fd75cd28
ouyangziyi <ziyi93@yeah.net>
2023-04-12 00:28:28 +0800

    add 1

---
+++ 6/6_3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+
+void reverse_string( char *string );
+
+
+void main() {
+    char str[] = "abcdefg";
+    printf("%s\n", str);
+    reverse_string(str);
+    printf("%s\n", str);
+}
+
+
+void reverse_string( char *string ) {
+    if (string == NULL) {
+        return;
+    }
+    int str_len = strlen(string);
+    int i, j;
+    char c;
+    for (i = 0; i < str_len / 2; i++) {
+        j = str_len - i -1;
+        // printf("%d => %d\n", i, j);
+        c = string[i];
+        string[i] = string[j];
+        string[j] = c;
+    }
+}
