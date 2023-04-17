ab799ea5b687716e84a7ed952ad5375935567d83
ouyangziyi <ziyi93@yeah.net>
2023-04-17 23:43:27 +0800

    add 2

---
+++ 6/6_4.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define USING_TYPE short int
+// todo: 获取USING_TYPE所有位为1/0
+#define ALLSET 0xffff
+#define ALLUNSET 0x0000
+
+
+void eratosthenes_6_4(int n);
+void eratosthenes_6_5(int n);
+USING_TYPE is_set(USING_TYPE *short_int_arr, int int_index, int bit_index);
+void set_bit(USING_TYPE *short_int_arr, int int_index, int bit_index);
+void unset_bit(USING_TYPE *short_int_arr, int int_index, int bit_index);
+
+
+void eratosthenes_6_4(int n) {
+    bool *is_prime = malloc(sizeof(bool) * (n + 1));
+    for (int i = 2; i <= n; i++) {
+        is_prime[i] = true;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+    printf("2 ");
+    for (int i = 3; i <= n; i += 2) {
+        if (is_prime[i]) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    free(is_prime);
+}
+
+
+USING_TYPE is_set(USING_TYPE *short_int_arr, int int_index, int bit_index) {
+    USING_TYPE n = short_int_arr[int_index];
+    n = n >> bit_index;
+    return n & 1;
+}
+
+
+void set_bit(USING_TYPE *short_int_arr, int int_index, int bit_index) {
+    USING_TYPE p = 1 << bit_index;
+    short_int_arr[int_index] = short_int_arr[int_index] | p;
+}
+
+
+void unset_bit(USING_TYPE *short_int_arr, int int_index, int bit_index) {
+    USING_TYPE p = ALLSET;
+    USING_TYPE m = 1 << bit_index;
+    p = p ^ m;
+    short_int_arr[int_index] = short_int_arr[int_index] & p;
+}
+
+
+void eratosthenes_6_5(int n) {
+    unsigned int s = sizeof(USING_TYPE);
+    unsigned int b = s * 8;
+    unsigned int num = n / b + 1;
+    USING_TYPE *short_int_arr = malloc(sizeof(USING_TYPE) * num);
+    for (int i = 0; i < num; i++) {
+        short_int_arr[i] = ALLSET;
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        int int_index = i / b;
+        int bit_index = i % b;
+        if (is_set(short_int_arr, int_index, bit_index)) {
+            for (int j = i * i; j <= n; j += i) {
+                int int_index = j / b;
+                int bit_index = j % b;
+                unset_bit(short_int_arr, int_index, bit_index);
+            }
+        }
+    }
+
+    printf("2 ");
+    for (int i = 3; i <= n; i += 2) {
+        int int_index = i / b;
+        int bit_index = i % b;
+        if (is_set(short_int_arr, int_index, bit_index)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    free(short_int_arr);
+}
+
+
+void main() {
+    eratosthenes_6_4(100);
+    eratosthenes_6_5(100);
+}
